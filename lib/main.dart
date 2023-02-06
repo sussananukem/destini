81e9be0d36246cf6e30082d4ea173d579253fcde
Sussan T. Anukem <sussananukem20@gmail.com>
2023-02-06 15:17:47 +0000

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'story_brain.dart';
 
 void main() {
   runApp(
@@ -38,8 +39,85 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  StoryBrain storyBrain = StoryBrain();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Destini')),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                  'images/bg.jpg',
+                ),
+                fit: BoxFit.cover)),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+             children: [
+              Expanded(
+                flex: 12,
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Center(
+                    child: Text(
+                      storyBrain.getStory(),
+                      textAlign: TextAlign.justify,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                    child: Text(
+                      storyBrain.getChoiceOne(),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blueGrey[50],
+                    ),
+                    child: Text(
+                      storyBrain.getChoiceTwo(),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/story.dart
@@ -0,0 +1,7 @@
+class Story{
+  late String storyText;
+  late String choiceOne;
+  late String choiceTwo;
+
+  Story({required this.storyText, required this.choiceOne, required this.choiceTwo});
+}
